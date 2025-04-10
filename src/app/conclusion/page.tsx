43ed54844d645932c728739817c5b4b1608658e1
Jonathan Bangert <jonathan@bangert.dk>
2025-04-10 08:07:05 -0600

    gladwell

--- src/app/conclusion/page.tsx
+++ src/app/conclusion/page.tsx
@@ -18,26 +18,32 @@ export default function DurableSkills() {
 
       <Card>
         <CardHeader>
-          <CardTitle className="text-3xl">
-            Conclusion
-          </CardTitle>
+          <CardTitle className="text-3xl">Conclusion</CardTitle>
         </CardHeader>
         <CardContent>
-          <p className="text-lg">
-            conclusion here
-          </p>
+          <p className="text-lg">conclusion here</p>
         </CardContent>
       </Card>
 
       <Card>
         <CardHeader>
-          <CardTitle className="text-3xl">
-            Connection to Gladwell
-          </CardTitle>
+          <CardTitle className="text-3xl">Connection to Gladwell</CardTitle>
         </CardHeader>
         <CardContent>
           <p className="text-lg">
-            eyyyyy
+            In ‘Outliers’, Gladwell talks about how success isn’t just about
+            being smart or working hard. He emphasizes how important roles
+            opportunities play for an individual's success. One idea I really
+            resonated with was the term “Accumulative Advantage”. The idea that
+            a small headstart early on can grow into something much bigger.
+            Someone's education is probably the most important and early
+            opportunity one gets in life, and right now, the entire field of
+            education is being reshaped by a new technology, AI. Right now, many
+            students are learning to use AI, but some aren't, and many again are
+            using the technology to cheat and therefore jeopardise their own
+            education. Therefore I wanted to look into, and truly understand how
+            we can really integrate AI correctly, while preserving authentic
+            learning and creativity.
           </p>
         </CardContent>
       </Card>
